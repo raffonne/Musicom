fed09d4c84f89e90e21250a754581edb27b14cde
raffonne <kowalczuk.raf@gmail.com>
2022-06-01 01:15:45 +0200

    credits

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -107,3 +107,45 @@ let date = document.querySelector("#date");
 date.innerHTML = "©rafko2022-" + String(year + 1);
 
 
+//CARD Slider//
+$('[data-element="carousel"]').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+  customPaging : function(slider, i) {
+    return '<span></span>';
+  },
+});
+
+var windowSel = $(window);
+
+// parallax
+function parallax(selector, speed) {
+  var movement = -(windowSel.scrollTop() * (speed / 10));
+  $(selector).css('transform', 'translate3d(0,' + movement + 'px, 0');
+}
+
+// parallax init
+function parallaxInit(selector) {
+  if ($(selector).length && window.innerWidth > 1024 && navigator.userAgent.match(/iPad/i) == null) {
+    $(selector).each(function(i, el) {
+      var speed = $(el).attr('data-speed');
+
+      //init function on load
+      parallax($(el), speed);
+
+      // init function on scroll
+      windowSel.on('scroll', function() {
+        parallax($(el), speed);
+      });
+    });
+  }
+}
+
+var parallaxItem = '[data-parallax]';
+parallaxInit(parallaxItem);
+
+// according to effect from this website: http://www.techstyle.com/
+
